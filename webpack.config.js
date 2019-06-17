1ee4f14ec145ca1befcd635c003beb1f9666b45b
Simon Zünd <szuend@chromium.org>
2019-06-17 15:48:47 +0200

    Use webpack to bundle the extension

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// Copyright 2019 The VSCode V8 Torque Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json).
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    // the vscode-module is created on-the-fly and must be excluded.
+    // Add other modules that cannot be webpack'ed.
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files.
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
